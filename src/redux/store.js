21aaadf67ca3f85e28e947a0448fc2e4341687d5
Spaun1 <andreispaun7@gmail.com>
2023-05-05 21:50:33 +0300

    add store

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { legacy_createStore } from 'redux';
+import reducer from './reducers/reducer';
+
+const store = legacy_createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
