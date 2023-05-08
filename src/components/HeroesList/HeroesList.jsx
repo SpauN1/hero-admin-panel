4ad136d8618257d48757227e71cd38023451cbaf
Spaun1 <andreispaun7@gmail.com>
2023-05-08 22:14:35 +0300

    add character removal functionality

--- src/components/HeroesList/HeroesList.jsx
+++ src/components/HeroesList/HeroesList.jsx
@@ -1,19 +1,15 @@
 import { useHttp } from '../../hooks/http.hook';
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import {
   heroesFetching,
   heroesFetched,
   heroesFetchingError,
+  heroDeleted,
 } from '../../redux/actions/actions';
 import HeroesListItem from '../HeroesListItem/HeroesListItem';
 import Spinner from '../Spinner/Spinner';
-
-// Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
-// Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
   const { heroes, heroesLoadingStatus } = useSelector((state) => state);
@@ -25,9 +21,19 @@ const HeroesList = () => {
     request('http://localhost:3001/heroes')
       .then((data) => dispatch(heroesFetched(data)))
       .catch(() => dispatch(heroesFetchingError()));
-
     // eslint-disable-next-line
   }, []);
+
+  const onDelete = useCallback(
+    (id) => {
+      request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+        .then((data) => console.log(data, 'Deleted'))
+        .then(dispatch(heroDeleted(id)))
+        .catch((err) => console.log(err));
+    },
+    // eslint-disable-next-line
+    [request]
+  );
 
   if (heroesLoadingStatus === 'loading') {
     return <Spinner />;
@@ -41,7 +47,7 @@ const HeroesList = () => {
     }
 
     return arr.map(({ id, ...props }) => {
-      return <HeroesListItem key={id} {...props} />;
+      return <HeroesListItem {...props} onDelete={() => onDelete(id)} />;
     });
   };
 
