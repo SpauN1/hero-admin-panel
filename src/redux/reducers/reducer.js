b425ed4c5746f747ae71396d1ca6af22be6d9835
Spaun1 <andreispaun7@gmail.com>
2023-05-09 04:21:46 +0300

    add reducer create hero

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -23,6 +23,18 @@ const reducer = (state = initialState, action) => {
         ...state,
         heroesLoadingStatus: 'error',
       };
+    case 'HERO_CREATED':
+      let newCreatedHeroList = [...state.heroes, action.payload];
+      return {
+        ...state,
+        heroes: newCreatedHeroList,
+        filteredHeroes:
+          state.activeFilter === 'all'
+            ? newCreatedHeroList
+            : newCreatedHeroList.filter(
+                (item) => item.element === state.activeFilter
+              ),
+      };
     case 'HERO_DELETED':
       const newHeroList = state.heroes.filter(
         (item) => item.id !== action.payload
