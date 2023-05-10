3d92a12ee2ca8d3bff40d554b344ee1f25470cbb
Spaun1 <andreispaun7@gmail.com>
2023-05-11 02:53:47 +0300

    add  filter to the hero by elements of nature

--- src/components/HeroesList/HeroesList.jsx
+++ src/components/HeroesList/HeroesList.jsx
@@ -12,7 +12,7 @@ import HeroesListItem from '../HeroesListItem/HeroesListItem';
 import Spinner from '../Spinner/Spinner';
 
 const HeroesList = () => {
-  const { heroes, heroesLoadingStatus } = useSelector((state) => state);
+  const { filteredHeroes, heroesLoadingStatus } = useSelector((state) => state);
   const dispatch = useDispatch();
   const { request } = useHttp();
 
@@ -51,7 +51,7 @@ const HeroesList = () => {
     });
   };
 
-  const elements = renderHeroesList(heroes);
+  const elements = renderHeroesList(filteredHeroes);
   return <ul>{elements}</ul>;
 };
 
