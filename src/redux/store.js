9c9a656ef0403ccf552a2fe0863289c3eff808dc
Spaun1 <andreispaun7@gmail.com>
2023-05-18 14:33:39 +0300

    add Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,57 @@
-import { legacy_createStore, combineReducers, compose } from 'redux';
+import {
+  legacy_createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from 'redux';
 
 import heroes from './reducers/heroes';
 import filters from './reducers/filters';
 
-const enhancer =
-  (createStore) =>
-  (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-      if (typeof action === 'string') {
-        return oldDispatch({
-          type: action,
-        });
-      }
-      return oldDispatch(action);
-    };
-    return store;
-  };
+const stringMiddleware = (store) => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action,
+    });
+  }
+  return next(action);
+};
 
 const store = legacy_createStore(
   combineReducers({ heroes: heroes, filters: filters }),
+  // applyMiddleware(stringMiddleware)
+  // Подключаем DevTools
   compose(
-    enhancer,
+    applyMiddleware(stringMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
 export default store;
+
+// Store Enhancers
+
+// const enhancer =
+//   (createStore) =>
+//   (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//       if (typeof action === 'string') {
+//         return oldDispatch({
+//           type: action,
+//         });
+//       }
+//       return oldDispatch(action);
+//     };
+//     return store;
+//   };
+
+// const store = legacy_createStore(
+//   combineReducers({ heroes: heroes, filters: filters }),
+//   compose(
+//     enhancer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
